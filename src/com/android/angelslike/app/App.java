d6cfc520eba606e7405202e1e09437af01716d1b
hezongfa <651780771@qq.com>
2015-08-30 16:46:41 +0800

    1

--- src/com/android/angelslike/app/App.java
+++ src/com/android/angelslike/app/App.java
@@ -1,11 +1,12 @@
 package com.android.angelslike.app;
 
 public class App extends com.hzf.app.App {
-
-	@Override
-	public void onCreate() {
-		// TODO Auto-generated method stub
-		super.onCreate();
-	}
-
+    
+    @Override
+    public void onCreate() {
+        // TODO Auto-generated method stub
+        super.onCreate();
+        // afafafafs
+    }
+    
 }
